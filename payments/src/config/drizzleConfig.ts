cafbe8cbbd9f8ef37187ab6e9f3a7f0487526351
Faheem-T <faheemmbasheer@gmail.com>
2025-07-21 11:50:25 +0530

    rabbit mq, payments service and common module setup

---
+++ payments/src/config/drizzleConfig.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { ENV } from "./dotenv";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "src/infrastructure/db/schemas",
+  dbCredentials: {
+    url: ENV.DATABASE_URL,
+    user: "postgres",
+  },
+});
