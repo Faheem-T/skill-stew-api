4f228c5c1e6154d7e6b4c9bfe7612222e398fdd4
Faheem-T <faheemmbasheer@gmail.com>
2025-07-29 18:13:50 +0530

    [common] change auth middleware to send back error response

--- common/src/middlewares/authMiddleware.ts
+++ common/src/middlewares/authMiddleware.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import { UnauthenticatedError } from "../errors/UnauthenticatedError";
 import { JwtHelper } from "../jwt-utils/JwtHelper";
+import { AccessTokenVerifyError, InvalidTokenError } from "../errors/JwtErrors";
+import { HttpStatus } from "../constants/HttpStatus";
 
 export class AuthMiddleware {
   private _jwtHelper: JwtHelper;
@@ -17,12 +19,13 @@ export class AuthMiddleware {
     });
   }
 
-  verify: RequestHandler = (req, _res, next) => {
+  verify: RequestHandler = (req, res, next) => {
     try {
       const token = req.headers["authorization"]?.split(" ")[1];
       if (!token) {
         throw new UnauthenticatedError();
       }
+
       const payload = this._jwtHelper.verifyAccessToken(token);
       req.user = {
         id: payload.userId,
@@ -33,6 +36,22 @@ export class AuthMiddleware {
       };
       next();
     } catch (err) {
+      if (err instanceof AccessTokenVerifyError) {
+        res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ success: false, message: err.message, code: err.code });
+        return;
+      } else if (err instanceof InvalidTokenError) {
+        res
+          .status(HttpStatus.UNAUTHORIZED)
+          .json({ success: false, message: err.message, code: err.code });
+        return;
+      } else if (err instanceof UnauthenticatedError) {
+        res
+          .status(HttpStatus.UNAUTHORIZED)
+          .json({ success: false, message: err.message, code: err.code });
+        return;
+      }
       next(err);
     }
   };
