2ef30e9c3357cb122360eef270be7dfe0c53ffd1
Faheem-T <faheemmbasheer@gmail.com>
2025-09-18 21:27:58 +0530

    feat: Create ES client in es proxy

--- es-proxy/src/index.ts
+++ es-proxy/src/index.ts
@@ -3,6 +3,7 @@ import amqp from "amqplib";
 import { ENV } from "./utils/dotenv.js";
 import { logger } from "./utils/logger/index.js";
 import { Client } from "@elastic/elasticsearch";
+import { ESClient } from "./ESClient.js";
 
 const connection = await amqp.connect(
   `amqp://${ENV.RABBITMQ_USER}:${ENV.RABBITMQ_PASSWORD}@my-rabbit`,
@@ -11,15 +12,27 @@ const channel = await connection.createChannel();
 
 const consumer = new Consumer();
 
-await consumer.init(channel, "stew_exchange", "es_proxy", ["user.registered"]);
+await consumer.init(channel, "stew_exchange", "es_proxy", [
+  "user.registered",
+  "user.verified",
+]);
+logger.info("Event consumer set up.");
+
+const es = new Client({ node: ENV.ES_URL });
+const esClient = new ESClient(es);
+await esClient.init();
+logger.info("ES client set up.");
+
 consumer.registerHandler("user.registered", async (event) => {
-  logger.info("Handling user registered event!", event);
+  const { email, id } = event.data;
+  await esClient.index("users", { email, id }, id);
   return { success: true };
 });
 
-logger.info("Event consumer set up.");
+consumer.registerHandler("user.verified", async (event) => {
+  const { id } = event.data;
 
-logger.info("ES URL", ENV.ES_URL);
-const es = new Client({ node: ENV.ES_URL });
-const esInfo = await es.info();
-logger.info(esInfo);
+  await esClient.updateUser(id, { isVerified: true });
+
+  return { success: true };
+});
