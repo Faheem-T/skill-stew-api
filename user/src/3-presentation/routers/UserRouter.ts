feddc63d6f5b320b698df0321615464c6afdf8d0
Faheem-T <faheemmbasheer@gmail.com>
2025-09-18 13:19:14 +0530

    setup bare bones es-proxy

--- user/src/3-presentation/routers/UserRouter.ts
+++ user/src/3-presentation/routers/UserRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { requireRole } from "../middlewares/requireRole";
-import { userController } from "../../di";
+import { producer, userController } from "../../di";
+import { CreateEvent } from "@skillstew/common";
 
 const router = Router();
 
@@ -11,5 +12,16 @@ router.patch("/:id/block", requireRole("ADMIN"), userController.blockUser);
 router.patch("/:id/unblock", requireRole("ADMIN"), userController.unblockUser);
 
 router.post("/dummy", requireRole("ADMIN"), userController.createDummyUsers);
+
+router.post("/emit", (req, res, next) => {
+  try {
+    const { id, email } = req.body;
+    const event = CreateEvent("user.registered", { id, email }, "user");
+    producer.publish(event);
+    res.status(200).json({ message: "Event published" });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
