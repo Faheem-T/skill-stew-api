bf5c32e7a2474bee567c46b49132680d907c3229
Faheem-T <faheemmbasheer@gmail.com>
2025-11-07 12:05:22 +0530

    feat(common): add user profile updated event

--- common/src/events/schemas/userEventsSchema.ts
+++ common/src/events/schemas/userEventsSchema.ts
@@ -9,7 +9,16 @@ export const userVerifiedSchema = z.object({
   id: z.uuid(),
 });
 
+export const userProfileUpdatedSchema = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  username: z.string(),
+  location: z.object({ latitude: z.number(), longitude: z.number() }),
+  languages: z.array(z.string()),
+});
+
 export const UserEventSchemas = {
   "user.registered": userRegisteredSchema,
   "user.verified": userVerifiedSchema,
+  "user.profileUpdated": userProfileUpdatedSchema,
 } as const;
