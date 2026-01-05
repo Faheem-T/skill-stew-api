f2be2bf6c86eea640f1ea5e2772768940ae3d56e
Faheem M. <faheemmbasheer@gmail.com>
2026-01-05 09:22:37 +0530

    feat: add infisical to skill service

--- skill/src/start.ts
+++ skill/src/start.ts
@@ -3,15 +3,11 @@ import { connectDB } from "./config/mongoConnection";
 import { ENV } from "./utils/dotenv";
 import { logger } from "./utils/logger";
 
-async function start() {
-  try {
-    await connectDB();
-    app.listen(ENV.PORT);
-    logger.info(`Listening on port ${ENV.PORT}`);
-  } catch (err) {
-    logger.error(err);
-    throw err;
-  }
+try {
+  await connectDB();
+  app.listen(ENV.PORT);
+  logger.info(`Listening on port ${ENV.PORT}`);
+} catch (err) {
+  logger.error(err);
+  throw err;
 }
-
-start();
