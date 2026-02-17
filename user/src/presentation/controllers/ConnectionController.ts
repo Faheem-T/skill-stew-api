d8e168373e5e5b0af3e75d9a4656beb4b206796c
Faheem M. <faheemmbasheer@gmail.com>
2026-02-17 15:27:45 +0530

    feat(user-service): add connection controller

---
+++ user/src/presentation/controllers/ConnectionController.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { IAcceptConnection } from "../../application/interfaces/user/IAcceptConnection";
+import { IRejectConnection } from "../../application/interfaces/user/IRejectConnection";
+import { ISendConnectionRequest } from "../../application/interfaces/user/ISendConnectionRequest";
+import { ForbiddenError } from "../../domain/errors/ForbiddenError";
+import { ValidationError } from "../../application/errors/ValidationError";
+
+export class ConnectionController {
+  constructor(
+    private _sendConnectionRequest: ISendConnectionRequest,
+    private _acceptConnection: IAcceptConnection,
+    private _rejectConnection: IRejectConnection,
+  ) {}
+
+  sendConnectionRequest = async (
+    req: Request<{ userId: string | undefined }>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const userId = req.headers["x-user-id"] as string;
+
+      if (!userId) {
+        throw new ForbiddenError();
+      }
+
+      const recipientId = req.params.userId;
+
+      if (!recipientId) {
+        throw new ValidationError([{ message: "recipientId is required" }]);
+      }
+
+      await this._sendConnectionRequest.exec(userId, recipientId);
+    } catch (err) {
+      next(err);
+    }
+  };
+}
