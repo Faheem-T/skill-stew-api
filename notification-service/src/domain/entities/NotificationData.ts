e781c83182e6a88cb8d8e19b2da5164d7c0f98c0
Faheem M. <faheemmbasheer@gmail.com>
2026-02-24 15:00:27 +0530

    fix(notif-srv): make usernames in connection notification optional

--- notification-service/src/domain/entities/NotificationData.ts
+++ notification-service/src/domain/entities/NotificationData.ts
@@ -3,21 +3,21 @@ import { NotificationType } from "./NotificationType.enum";
 export interface ConnectionRequestData {
   type: NotificationType.CONNECTION_REQUEST;
   senderId: string;
-  senderUsername: string;
+  senderUsername: string | undefined;
   connectionId: string;
 }
 
 export interface ConnectionAcceptedData {
   type: NotificationType.CONNECTION_ACCEPTED;
   accepterId: string;
-  accepterUsername: string;
+  accepterUsername: string | undefined;
   connectionId: string;
 }
 
 export interface ConnectionRejectedData {
   type: NotificationType.CONNECTION_REJECTED;
   rejecterId: string;
-  rejecterUsername: string;
+  rejecterUsername: string | undefined;
   connectionId: string;
 }
 
