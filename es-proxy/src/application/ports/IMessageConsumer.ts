54b0738f45a2360fff2f24aed6b9c889cfb01971
Faheem M. <faheemmbasheer@gmail.com>
2026-02-25 10:25:49 +0530

    fix: make registerHandler async for event consumer interface

--- es-proxy/src/application/ports/IMessageConsumer.ts
+++ es-proxy/src/application/ports/IMessageConsumer.ts
@@ -10,5 +10,5 @@ export interface IMessageConsumer {
   registerHandler: <T extends EventName>(
     eventName: T,
     handler: (event: AppEvent<T>) => Promise<HandlerResult>,
-  ) => void;
+  ) => Promise<void>;
 }
