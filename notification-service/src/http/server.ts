bf18e4c13dd921018ce538e2a4990df8b7f6dff1
Faheem M. <faheemmbasheer@gmail.com>
2026-02-28 20:17:29 +0530

    feat(notif-srv): add httpLogger middleware

--- notification-service/src/http/server.ts
+++ notification-service/src/http/server.ts
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import express from "express";
 import notificationRouter from "./routers/NotificationRouter";
 import { errorHandler } from "./middlewares/errorHandler";
+import { httpLogger } from "./middlewares/httpLogger";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(httpLogger);
 
 app.use("/api/v1/notifications", notificationRouter);
 
