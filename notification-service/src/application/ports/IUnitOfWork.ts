03dcd21d10c1ef78bec5c1dfe4ccb971c3ceb2f5
Faheem M. <faheemmbasheer@gmail.com>
2026-02-28 17:28:41 +0530

    feat(notif-srv): add UnitOfWork

---
+++ notification-service/src/application/ports/IUnitOfWork.ts
@@ -0,0 +1,5 @@
+import type { TransactionContext } from "../../types/TransactionContext";
+
+export interface IUnitOfWork {
+  transact<T>(work: (tx: TransactionContext) => Promise<T>): Promise<T>;
+}
