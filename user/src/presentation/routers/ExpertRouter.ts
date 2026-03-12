a0384268f3b3b9a50dcaba1b38702e48b2debd5b
Faheem M. <faheemmbasheer@gmail.com>
2026-03-12 11:47:00 +0530

    fix(user-srv): remove requireRole from /expert/apply route

--- user/src/presentation/routers/ExpertRouter.ts
+++ user/src/presentation/routers/ExpertRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { expertController } from "../../di";
-import { requireRole } from "../middlewares/requireRole";
 
 const router = Router();
 
 // api/v1/experts
 
-router.post("/apply", requireRole("USER"), expertController.apply);
+router.post("/apply", expertController.apply);
 
 export default router;
