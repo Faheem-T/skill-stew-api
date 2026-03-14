87e9ae83c54757b6e7d89a90741e4073d6191850
Faheem M. <faheemmbasheer@gmail.com>
2026-03-14 12:45:04 +0530

    feat: add expert profile entity and schema

--- user/src/domain/entities/ExpertProfile.ts
+++ user/src/domain/entities/ExpertProfile.ts
@@ -1,7 +1,33 @@
 export class ExpertProfile {
-  constructor(
-    public expertId: string,
-    public avatarKey?: string,
-    // TODO: Add needed fields
-  ) {}
+  public expertId: string;
+  public fullName: string;
+  public phone: string;
+  public socialLinks: string[];
+  public bio: string;
+
+  // Expertise
+  public yearsExperience: number;
+  public hasTeachingExperience: boolean;
+  public teachingExperienceDesc?: string;
+
+  public avatarKey?: string;
+  public bannerKey?: string;
+  public languages: string[];
+
+  public joinedAt: Date;
+
+  constructor(args: ExpertProfile) {
+    this.expertId = args.expertId;
+    this.fullName = args.fullName;
+    this.phone = args.phone;
+    this.socialLinks = args.socialLinks;
+    this.bio = args.bio;
+    this.yearsExperience = args.yearsExperience;
+    this.hasTeachingExperience = args.hasTeachingExperience;
+    this.teachingExperienceDesc = args.teachingExperienceDesc;
+    this.avatarKey = args.avatarKey;
+    this.bannerKey = args.bannerKey;
+    this.languages = args.languages;
+    this.joinedAt = args.joinedAt;
+  }
 }
