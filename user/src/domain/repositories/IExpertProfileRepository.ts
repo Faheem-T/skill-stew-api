0fd36af5e69aca9381a501d5c315b5b4345ff6ea
Faheem M. <faheemmbasheer@gmail.com>
2026-03-24 20:22:42 +0530

    feat(user-srv): add `getByExpertId` method to expert profile repository

--- user/src/domain/repositories/IExpertProfileRepository.ts
+++ user/src/domain/repositories/IExpertProfileRepository.ts
@@ -1,4 +1,10 @@
+import { TransactionContext } from "../../types/TransactionContext";
 import { ExpertProfile } from "../entities/ExpertProfile";
 import { IBaseRepository } from "./IBaseRepository";
 
-export interface IExpertProfileRepository extends IBaseRepository<ExpertProfile> {}
+export interface IExpertProfileRepository extends IBaseRepository<ExpertProfile> {
+  findByExpertId(
+    expertId: string,
+    tx?: TransactionContext,
+  ): Promise<ExpertProfile>;
+}
