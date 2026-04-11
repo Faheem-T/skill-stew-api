a87757c6a60adaf302c778e3927a67a7b07c9548
Faheem M. <faheemmbasheer@gmail.com>
2026-04-11 13:07:58 +0530

    feat(payment): add UnitOfWork

---
+++ payments/src/infrastructure/persistence/UnitOfWork.ts
@@ -0,0 +1,11 @@
+import type { IUnitOfWork } from "../../application/ports/IUnitOfWork";
+import { db } from "../../start";
+import type { TransactionContext } from "../../types/TransactionContext";
+
+export class UnitOfWork implements IUnitOfWork {
+  async transact<T>(work: (tx: TransactionContext) => Promise<T>): Promise<T> {
+    return db.transaction(async (tx) => {
+      return work(tx);
+    });
+  }
+}
